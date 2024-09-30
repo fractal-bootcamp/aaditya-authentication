4e3bdd9776d1a4bedf35af6965b5cebad942d9a6
Aaditya Desai <aadityadesai09@gmail.com>
2024-09-30 19:17:02 -0400

    added clerk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
-import './index.css';
+
+// Your Clerk publishable key
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ClerkProvider publishableKey={clerkPublishableKey}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ClerkProvider>
 );
